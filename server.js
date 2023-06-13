d7a13bed4401e6bfa6e1237b187d14001b794895
Jayesh Sadhwani <69295377+jayeshsadhwani99@users.noreply.github.com>
2023-06-13 18:07:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,10 @@ const path = require('path');
 
 const app = express();
 
-const answer = require("./index.js");
-
 app.listen(3000, () => {
   console.log('server started');
 });
 
 app.get('/', (req, res) => {
-  res.send(answer("SERR YBIR? NPPVBWBO"))
+  res.send("Hello world");
 });
